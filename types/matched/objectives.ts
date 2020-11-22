399f66d43d9a02de411265403692810937ef6291
temur.navoi <temur.navoi@yandex.com>
2020-11-22 20:29:16 +0500

    - added classes for objectives

---
+++ types/matched/objectives.ts
@@ -0,0 +1,42 @@
+class Objective {
+  id: number
+  name: string
+  description: string
+  type: ObjectiveType
+
+  constructor (id: number, name: string, description: string, type: ObjectiveType) {
+    this.id = id
+    this.name = name
+    this.description = description
+    this.type = type
+  }
+}
+
+enum ObjectiveType {
+  PRIMARY = 'PRIMARY',
+  SECONDARY = 'SECONDARY'
+}
+
+export class PrimaryObjective extends Objective {
+  public maxPoints: number = 45
+
+  constructor (id: number, name: string, description: string, maxPoints?: number) {
+    super(id, name, description, ObjectiveType.PRIMARY)
+
+    if (maxPoints) { this.maxPoints = maxPoints }
+  }
+}
+
+export enum ScoringType {
+  PROGRESSIVE,
+  END_GAME
+}
+
+export class SecondaryObjective extends Objective {
+  public scoringType: ScoringType
+
+  constructor (id: number, name: string, description: string, scoringType: ScoringType) {
+    super(id, name, description, ObjectiveType.SECONDARY)
+    this.scoringType = scoringType
+  }
+}
